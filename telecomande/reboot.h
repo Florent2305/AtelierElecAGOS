3064a90eea7d8f188e8b494765de6806b8969d52
Jeremy Lefort-Besnard <21123003+JLefortBesnard@users.noreply.github.com>
2024-03-05 14:30:21 +0100

    Add french comments

--- telecomande/reboot.h
+++ telecomande/reboot.h
@@ -1,15 +1,16 @@
-#pragma once
-#ifndef reboot_h
-#define reboot_h
+#pragma once // Directive pour garantir l'inclusion unique de ce fichier d'en-tête
 
-#include <avr/wdt.h>
- 
- 
+#ifndef reboot_h // Directive pour l'inclusion conditionnelle de ce fichier d'en-tête
+#define reboot_h // Directive pour marquer le début de la définition du fichier d'en-tête
+
+#include <avr/wdt.h> // Inclusion de la bibliothèque AVR pour utiliser les fonctionnalités du watchdog
+
+// Fonction pour redémarrer le microcontrôleur
 void reboot()
 {
-	Serial.println(F("Reboot asked"));
-    wdt_enable(WDTO_15MS); // activer le watchdog
-    while (1) {};          // et attendre ...
+    Serial.println(F("Reboot demandé")); // Affiche un message indiquant que le redémarrage est demandé
+    wdt_enable(WDTO_15MS); // Active le watchdog avec un délai de 15 millisecondes
+    while (1) {}; // Boucle infinie pour attendre le redémarrage
 }
 
-#endif
+#endif // Fin de la définition du fichier d'en-tête reboot_h
